a1b77057cce3ff57ffcb6dc70313b424db5951da
Jeisson Gómez <jeypitbullz4@gmail.com>
2023-12-18 08:15:59 -0500

    Actualizacion del proyecto y un nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+//Componentes
+import { HomeComponent } from './Componentes/home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    HomeComponent,
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//componentes
+import { EstudiosComponent } from './Componentes/estudios/estudios.component';
+import { HomeComponent } from './Componentes/home/home.component';
+
+export const routes: Routes = [
+    { path: 'Home', component: HomeComponent },
+    { path: 'Estudios', component: EstudiosComponent },
+];
